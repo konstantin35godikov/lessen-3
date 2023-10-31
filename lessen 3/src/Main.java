cb8ad6c227c5f884bdb1da1bf2e1882fbf9ad989
User <martemjanovkonstantin@gmail.com>
2023-10-31 22:38:07 +0600

    наконец то чето получается

---
+++ lessen 3/src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Приветствуем новых членов на наших уроках по программированию!!!");
+//                System.out.println("\t\t\t добро пожаловать в конвертер гигабайтов");
+//        System.out.println("В "   + 5 +  " ГБ: " + ( 5*1024 ) +  " МБ ");
+//
+////        масса(кг)/рост(м2)
+//      System.out.println("\t\t\t*** Добро пожалоапть в калькулятор BMI");
+//        System.out.println("Введите ");
+
+////           F="C " * (1.8+32)
+//        System.out.println("Введите температуру по цельсию:");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println( a + " градуса по цельсию это " +(a * 1.8+32)+ " градуса по фаренгейту");
+
+
+//        System.out.println("\t\t\t*** Добро пожалоапть в конвертер валют");
+//        System.out.println("Введите количество рублей:");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println(a + " рублей это " +(a *(1/62.0))+ " долларов");
+//        System.out.println(a + " рублей это " +(a *(1/65.0))+ " евро");
+
+
+        System.out.println("Java это пиздец как тяжело, " +
+                "Я заебался уже блядь!!!");
+
+        System.out.println("Добрый вечер Денис!");
+        System.out.println("Сколь вам полных лет:");
+        int a  = new Scanner(System.in).nextInt();
+        System.out.println( a + " года, это значит что ты Дениска пиписка!!!");
+//        int a = 5;
+//        int b = 6;
+//        int catCount = 8;
+//        int driftStiv = 9;
+//        int nuwHappu = 10;
+//
+//        String s = "some test;
+//        char dog = 'в';
+//        char cat = 'к';
+//        int c = 10;
+//        double v = 1.75;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(catCount);
+//        System.out.println(driftStiv);
+//        System.out.println(nuwHappu);
+//        System.out.println(l);
+//        System.out.println(dog);
+//        System.out.println(cat);
+//        System.out.println(c);
+//        System.out.println(v);
+//        System.out.println("\t\t\t***Добро пожаловать в калькулятор умножения!***\n\n");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число");
+//        double b = new Scanner(System.in).nextDouble();
+//        System.out.println(a + " * " + b + " = " + a *  b);
+//
+//        System.out.println("\t\t\t***Добро пожаловать в калькулятор суммы!***\n\n");
+//        System.out.println("Введите первое число:");
+//        double ab = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число");
+//        double bb = new Scanner(System.in).nextDouble();
+//        System.out.println(ab + " + " + bb + " = " + (ab +  bb));
+
+
+//        System.out.println("\t\t\t***Добро пожаловать в калькулятор частного!***\n\n");
+//        System.out.println("Введите первое число:");
+//        double aс = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число");
+//        double bс = new Scanner(System.in).nextDouble();
+//        System.out.println(aс + " - " + bс + " = " + (aс /  bс));
+
+
+    }
+}
+
